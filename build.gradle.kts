694eadc15b68adf14c595e599203fb53d291f51a
ccsimenson <ccsimenson@proton.me>
2025-03-31 15:24:25 -0600

    Building with Adroid Studo recommended changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.9.0" apply false
     id("com.android.library") version "8.9.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
 }
@@ -10,7 +10,7 @@ plugins {
 buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.9.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
     }
